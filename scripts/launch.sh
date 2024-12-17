b867bb158555254002c0e66c115fe54260c614ba
Frank Hereford <frank@frankhereford.com>
2024-12-17 10:01:54 -0600

    remove cron scripts

--- scripts/launch.sh
+++
@@ -1,9 +0,0 @@
-docker run --name  cctv-service \
-    -d \
-    -p 8000:8000 \
-    -e LANG=C.UTF-8 \
-    -v "/srv/publisher/atd-cctv-service/app":/app \
-    -v "/srv/publisher/atd-data-publishing/transportation-data-publishing/data":/data/ \
-    --name  cctv-service \
-    atddocker/cctv-service \
-    python app.py
